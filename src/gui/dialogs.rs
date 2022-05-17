077808fc53df07a0e14ca0d7dcea3567e6cfce79
Ian Douglas Scott <idscott@system76.com>
2022-05-16 20:13:36 -0700

    Move about/info dialog code into their own file

---
+++ src/gui/dialogs.rs
@@ -0,0 +1,108 @@
+use gtk4::prelude::*;
+use relm4::view;
+
+use crate::util;
+
+pub fn show_about_dialog(main_window: &gtk4::ApplicationWindow) {
+    gtk4::AboutDialog::builder()
+        .transient_for(main_window)
+        .modal(true)
+        .version(env!("CARGO_PKG_VERSION"))
+        .logo_icon_name("input-mouse-symbolic") // TODO
+        .copyright("Copyright 2022 Hewlett-Packard Development Company, L.P.")
+        .license_type(gtk4::License::MitX11)
+        .build()
+        .show()
+}
+
+fn device_to_model(device: &str) -> &str {
+    if device == "Brain" {
+        "HP 930 series Creator Wireless Mouse"
+    } else {
+        device
+    }
+}
+
+pub fn show_info_dialog(
+    main_window: &gtk4::ApplicationWindow,
+    device: &str,
+    serial: &str,
+    firmware_version: Option<(u16, u16, u16)>,
+) {
+    view! {
+        dialog = gtk4::Dialog {
+            set_transient_for: Some(main_window),
+            set_title: Some("About This Mouse"),
+            set_child = Some(&gtk4::ListBox) {
+                set_margin_start: 12,
+                set_margin_end: 12,
+                set_margin_top: 12,
+                set_margin_bottom: 12,
+                add_css_class: "frame",
+                set_header_func: util::header_func,
+                append = &gtk4::ListBoxRow {
+                    set_margin_start: 6,
+                    set_margin_end: 6,
+                    set_margin_top: 6,
+                    set_margin_bottom: 6,
+                    set_selectable: false,
+                    set_activatable: false,
+                    set_child = Some(&gtk4::Box) {
+                        set_spacing: 12,
+                        set_orientation: gtk4::Orientation::Horizontal,
+                        append = &gtk4::Label {
+                            set_label: "Model"
+                        },
+                        append = &gtk4::Label {
+                            set_label: device_to_model(device),
+                            set_hexpand: true,
+                            set_halign: gtk4::Align::End,
+                        }
+                    }
+                },
+                append = &gtk4::ListBoxRow {
+                    set_visible: firmware_version.is_some(),
+                    set_margin_start: 6,
+                    set_margin_end: 6,
+                    set_margin_top: 6,
+                    set_margin_bottom: 6,
+                    set_selectable: false,
+                    set_activatable: false,
+                    set_child = Some(&gtk4::Box) {
+                        set_spacing: 12,
+                        set_orientation: gtk4::Orientation::Horizontal,
+                        append = &gtk4::Label {
+                            set_label: "Firmware Version"
+                        },
+                        append = &gtk4::Label {
+                            set_label: &firmware_version.map_or_else(String::new, |(a, b, c)| format!("{}.{}.{}", a, b, c)),
+                            set_hexpand: true,
+                            set_halign: gtk4::Align::End,
+                        }
+                    }
+                },
+                append = &gtk4::ListBoxRow {
+                    set_margin_start: 6,
+                    set_margin_end: 6,
+                    set_margin_top: 6,
+                    set_margin_bottom: 6,
+                    set_selectable: false,
+                    set_activatable: false,
+                    set_child = Some(&gtk4::Box) {
+                        set_spacing: 12,
+                        set_orientation: gtk4::Orientation::Horizontal,
+                        append = &gtk4::Label {
+                            set_label: "Serial Number"
+                        },
+                        append = &gtk4::Label {
+                            set_label: serial,
+                            set_hexpand: true,
+                            set_halign: gtk4::Align::End,
+                        }
+                    }
+                }
+            }
+        }
+    }
+    dialog.show();
+}
--- src/gui/main.rs
+++ src/gui/main.rs
@@ -15,6 +15,8 @@ mod buttons_widget;
 use buttons_widget::{ButtonsWidget, BUTTONS, IMAGE_WIDTH};
 mod device_monitor_process;
 use device_monitor_process::DeviceMonitorProcess;
+mod dialogs;
+use dialogs::{show_about_dialog, show_info_dialog};
 mod keycode;
 mod profile;
 use profile::{apply_profile_diff, load_config, save_config, Binding, MouseConfig, MouseState};
@@ -632,15 +634,8 @@ impl Widgets<AppModel, ()> for AppWidgets {
         let device_group = RelmActionGroup::<DeviceActionGroup>::new();
 
         let about_action: RelmAction<AboutAction> =
-            RelmAction::new_stateless(glib::clone!(@strong sender, @weak main_window => move |_| {
-                gtk4::AboutDialog::builder()
-                    .transient_for(&main_window)
-                    .modal(true)
-                    .version(env!("CARGO_PKG_VERSION"))
-                    .logo_icon_name("input-mouse-symbolic") // TODO
-                    .copyright("Copyright 2022 Hewlett-Packard Development Company, L.P.")
-                    .license_type(gtk4::License::MitX11)
-                    .build().show()
+            RelmAction::new_stateless(glib::clone!(@weak main_window => move |_| {
+                show_about_dialog(&main_window);
             }));
         app_group.add_action(about_action);
 
@@ -782,98 +777,6 @@ impl Widgets<AppModel, ()> for AppWidgets {
 
         self.first_view_run = false;
     }
-}
-
-fn device_to_model(device: &str) -> &str {
-    if device == "Brain" {
-        "HP 930 series Creator Wireless Mouse"
-    } else {
-        device
-    }
-}
-
-fn show_info_dialog(
-    main_window: &gtk4::ApplicationWindow,
-    device: &str,
-    serial: &str,
-    firmware_version: Option<(u16, u16, u16)>,
-) {
-    view! {
-        dialog = gtk4::Dialog {
-            set_transient_for: Some(main_window),
-            set_title: Some("About This Mouse"),
-            set_child = Some(&gtk4::ListBox) {
-                set_margin_start: 12,
-                set_margin_end: 12,
-                set_margin_top: 12,
-                set_margin_bottom: 12,
-                add_css_class: "frame",
-                set_header_func: util::header_func,
-                append = &gtk4::ListBoxRow {
-                    set_margin_start: 6,
-                    set_margin_end: 6,
-                    set_margin_top: 6,
-                    set_margin_bottom: 6,
-                    set_selectable: false,
-                    set_activatable: false,
-                    set_child = Some(&gtk4::Box) {
-                        set_spacing: 12,
-                        set_orientation: gtk4::Orientation::Horizontal,
-                        append = &gtk4::Label {
-                            set_label: "Model"
-                        },
-                        append = &gtk4::Label {
-                            set_label: device_to_model(device),
-                            set_hexpand: true,
-                            set_halign: gtk4::Align::End,
-                        }
-                    }
-                },
-                append = &gtk4::ListBoxRow {
-                    set_visible: firmware_version.is_some(),
-                    set_margin_start: 6,
-                    set_margin_end: 6,
-                    set_margin_top: 6,
-                    set_margin_bottom: 6,
-                    set_selectable: false,
-                    set_activatable: false,
-                    set_child = Some(&gtk4::Box) {
-                        set_spacing: 12,
-                        set_orientation: gtk4::Orientation::Horizontal,
-                        append = &gtk4::Label {
-                            set_label: "Firmware Version"
-                        },
-                        append = &gtk4::Label {
-                            set_label: &firmware_version.map_or_else(String::new, |(a, b, c)| format!("{}.{}.{}", a, b, c)),
-                            set_hexpand: true,
-                            set_halign: gtk4::Align::End,
-                        }
-                    }
-                },
-                append = &gtk4::ListBoxRow {
-                    set_margin_start: 6,
-                    set_margin_end: 6,
-                    set_margin_top: 6,
-                    set_margin_bottom: 6,
-                    set_selectable: false,
-                    set_activatable: false,
-                    set_child = Some(&gtk4::Box) {
-                        set_spacing: 12,
-                        set_orientation: gtk4::Orientation::Horizontal,
-                        append = &gtk4::Label {
-                            set_label: "Serial Number"
-                        },
-                        append = &gtk4::Label {
-                            set_label: serial,
-                            set_hexpand: true,
-                            set_halign: gtk4::Align::End,
-                        }
-                    }
-                }
-            }
-        }
-    }
-    dialog.show();
 }
 
 relm4::new_action_group!(AppActionGroup, "app");
